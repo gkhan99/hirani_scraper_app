be2b6ceb7c3161fb90e67060c9f4dc859d165c71
gkhan99 <gkhan@hiranigroup.com>
2024-10-01 12:14:20 +0530

    Add files via upload

--- page6.py
+++ page6.py
@@ -3,9 +3,6 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-# Set full screen mode
-st.set_page_config(layout="wide")
-
 # Embed the website link in the title
 st.markdown(
     """
